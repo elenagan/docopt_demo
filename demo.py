97767a162d1f32424f647a9d9f2db724aaad2556
Elena Ganacheva <elena.ganacheva@gmail.com>
2022-11-18 11:10:46 -0800

    Added my version of demo.py

---
+++ demo.py
@@ -0,0 +1,17 @@
+# author: Tiffany Timbers
+# date: 2020-01-15
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> [<argnew>] --arg2=<arg2> [--arg3=<arg3>]
+
+Options:
+<arg1>            Takes any value (this is a required positional argument)
+[<argnew>]        Takes any value (this is a required optional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+""" 
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
